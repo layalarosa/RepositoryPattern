bec8b9c95881ae5a827a3800915498a407815203
layalarosa <layalarosa@gmail.com>
2022-03-27 15:33:54 -0400

    DTO implementation and code fixes.

---
+++ RepositoryPattern/Controllers/FileUploadController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RepositoryPattern.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileUploadController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public FileUploadController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [HttpPost("[action]")]
+        public IActionResult UploadFiles(List<IFormFile> files)
+        {
+            if (files.Count == 0)
+                return BadRequest();
+            string directoryPath = Path.Combine(_webHostEnvironment.ContentRootPath, "UploadFiles");
+
+            foreach (var file in files)
+            {
+                string filePath = Path.Combine(directoryPath, file.FileName);
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+            }
+            return Ok("Upload Sucessful");
+
+        }
+    }
+}
